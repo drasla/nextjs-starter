93a28c706620f3808e511c4a082cf99a4f796a12
Tae.Garam <drasla@naver.com>
2022-02-04 16:59:14 +0900

    #1.5 Styles JSX

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,15 @@ import NavBar from "../components/NavBar";
 export default function Home() {
     return (
         <div>
-            <NavBar />
-            <h1>Hello</h1>
+            <NavBar/>
+            <h1 className="active">Hello</h1>
+            <style jsx>
+                {`
+                  a {
+                    color: white;
+                  }
+                `}
+            </style>
         </div>
     );
 }
